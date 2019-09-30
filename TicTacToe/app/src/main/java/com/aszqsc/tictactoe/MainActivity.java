6c0182dd0d8929af562e841173ed7cbc831a33e5
HaoLaptop <37805188+abcpower123@users.noreply.github.com>
2019-09-30 20:12:29 +0700

    tictac toe ui

---
+++ TicTacToe/app/src/main/java/com/aszqsc/tictactoe/MainActivity.java
@@ -0,0 +1,21 @@
+package com.aszqsc.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToGame(View view) {
+        Intent t=new Intent(this,Game.class);
+        startActivity(t);
+    }
+}
